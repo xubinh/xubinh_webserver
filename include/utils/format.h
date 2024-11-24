beaaf7b664f19e949dce347764de1c43d5040860
xubinh <xubin.huang@outlook.com>
2024-11-24 17:03:03 +0800

    📝 Add TODO of implementing `Format`

---
+++ include/utils/format.h
@@ -0,0 +1,14 @@
+#ifndef XUBINH_SERVER_UTILS_FORMAT
+#define XUBINH_SERVER_UTILS_FORMAT
+
+namespace xubinh_server {
+
+namespace utils {
+
+namespace Format {}
+
+} // namespace utils
+
+} // namespace xubinh_server
+
+#endif
