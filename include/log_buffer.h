a83ae92995dcc0d50648710f59e6465f51e89cfd
xubinh <xubin.huang@outlook.com>
2024-11-25 22:56:25 +0800

    Adjust naming

--- include/log_buffer.h
+++ include/log_buffer.h
@@ -11,27 +11,17 @@ static constexpr std::size_t LOG_ENTRY_BUFFER_SIZE =
 static constexpr std::size_t LOG_CHUNK_BUFFER_SIZE =
     4 * 1000 * 1000; // 4 MB 的大缓冲区
 
+// declaration
 template <size_t LOG_BUFFER_SIZE>
 class LogBuffer {
-    // static_assert(
-    //     LOG_BUFFER_SIZE == LOG_ENTRY_BUFFER_SIZE
-    //         || LOG_BUFFER_SIZE == LOG_CHUNK_BUFFER_SIZE,
-    //     "Invalid log buffer size"
-    // );
-    // 将模板分离编译就已经足以用来限制模板实参的取值了
-
 private:
     char _buffer[LOG_BUFFER_SIZE];
-    char *_begin_position_of_available_area = _buffer;
-    const char *_end_position_of_buffer = _buffer + LOG_BUFFER_SIZE;
+    const char *_end_address_of_buffer = _buffer + LOG_BUFFER_SIZE - 1; // `\0`
+    char *_start_address_of_spare = _buffer;
 
     static constexpr size_t _MAX_BUFFER_SIZE = LOG_BUFFER_SIZE;
 
 public:
-    LogBuffer();
-
-    ~LogBuffer();
-
     // 无需拷贝, 想要新的缓冲区直接创建即可
     LogBuffer(const LogBuffer &) = delete;
     LogBuffer &operator=(const LogBuffer &) = delete;
@@ -41,22 +31,36 @@ public:
     LogBuffer(const LogBuffer &&) = delete;
     LogBuffer &operator=(const LogBuffer &&) = delete;
 
-    const char *get_begin_address_of_buffer() const;
+    LogBuffer() = default;
 
-    std::size_t get_size_of_written_area() {
-        return static_cast<size_t>(_begin_position_of_available_area - _buffer);
+    ~LogBuffer() = default;
+
+    const char *get_start_address_of_buffer() const {
+        return _buffer;
     }
 
-    std::size_t get_size_of_available_area() {
+    char *get_start_address_of_spare() {
+        return _start_address_of_spare;
+    }
+
+    std::size_t length() {
+        return static_cast<size_t>(_start_address_of_spare - _buffer);
+    }
+
+    std::size_t increment_length(const size_t &delta) {
+        return _start_address_of_spare += delta;
+    }
+
+    std::size_t length_of_spare() {
         return static_cast<size_t>(
-            _end_position_of_buffer - _begin_position_of_available_area
+            _end_address_of_buffer - _start_address_of_spare
         );
     }
 
     void append(const char *data, std::size_t data_size);
 
     void reset() {
-        _begin_position_of_available_area = _buffer;
+        _start_address_of_spare = _buffer;
     }
 
     static constexpr size_t capacity() const {
@@ -64,10 +68,7 @@ public:
     }
 };
 
-// explicit instantiation
-extern template class LogBuffer<LOG_ENTRY_BUFFER_SIZE>;
-extern template class LogBuffer<LOG_CHUNK_BUFFER_SIZE>;
-
+// helper declarations
 using LogEntryBuffer = LogBuffer<LOG_ENTRY_BUFFER_SIZE>;
 using LogChunkBuffer = LogBuffer<LOG_CHUNK_BUFFER_SIZE>;
 
--- src/log_buffer.cc
+++ src/log_buffer.cc
@@ -1,35 +1,24 @@
 #include <cstring> // std::memcpy
 
-#include "../include/log_buffer.h"
+#include "log_buffer.h"
 
 namespace xubinh_server {
-
-template <size_t LOG_BUFFER_SIZE>
-LogBuffer<LOG_BUFFER_SIZE>::LogBuffer() = default;
-
-template <size_t LOG_BUFFER_SIZE>
-LogBuffer<LOG_BUFFER_SIZE>::~LogBuffer() = default;
-
-template <size_t LOG_BUFFER_SIZE>
-const char *LogBuffer<LOG_BUFFER_SIZE>::get_begin_address_of_buffer() const {
-    return _buffer;
-}
 
 template <size_t LOG_BUFFER_SIZE>
 void LogBuffer<LOG_BUFFER_SIZE>::append(
     const char *data, std::size_t data_size
 ) {
     // 如果大小不够则直接丢弃:
-    if (data_size > get_size_of_available_area()) {
+    if (data_size > length_of_spare()) {
         return;
     }
 
-    std::memcpy(_begin_position_of_available_area, data, data_size);
+    std::memcpy(_start_address_of_spare, data, data_size);
 
-    _begin_position_of_available_area += data_size;
+    _start_address_of_spare += data_size;
 }
 
-// explicit instantiation:
+// explicit instantiation
 template class LogBuffer<LOG_ENTRY_BUFFER_SIZE>;
 template class LogBuffer<LOG_CHUNK_BUFFER_SIZE>;
 
