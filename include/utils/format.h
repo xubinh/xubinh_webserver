8f0686269a4ae2e0406b6c219f62e919984b2b09
xubinh <xubin.huang@outlook.com>
2024-11-25 22:51:14 +0800

    ✨ Add utility namespace `Format` for fast stringifying data

--- include/utils/format.h
+++ include/utils/format.h
@@ -1,11 +1,30 @@
 #ifndef XUBINH_SERVER_UTILS_FORMAT
 #define XUBINH_SERVER_UTILS_FORMAT
 
+#include <array>
+#include <cstddef>
+#include <type_traits>
+
 namespace xubinh_server {
 
 namespace utils {
 
-namespace Format {}
+namespace Format {
+
+// declaration
+template <typename T>
+int get_number_of_digits_of_type();
+// has to be non-constexpr since explicit instantiation of constexpr is not
+// allowed in C++11, and here I chose explicit instantiation over constexpr for
+// limiting possible tempalte argments
+
+// declaration
+template <typename T>
+size_t convert_integer_to_string_base_10(char *output_buffer, T input_value);
+
+void convert_pointer_to_hex_string(char *output_buffer, const void *pointer);
+
+} // namespace Format
 
 } // namespace utils
 
