4383061698be2e72e99b679222484dbd788acaae
xubinh <xubin.huang@outlook.com>
2024-11-27 20:21:33 +0800

    🚀 Pass the very first test of logging system (again)
    - The previous commit did nothing of testing since there was a bug within the configuration of cmake, i.e. `enable_testing()` must be put before `add_subdirectory(test)` in order to set up the testing framework.

--- test/test_logging.cc
+++ test/test_logging.cc
@@ -1,12 +1,16 @@
 #include <gtest/gtest.h>
 
 #include "log_builder.h"
+#include "log_collector.h"
 
 TEST(LogBuilderTest, BasicLogMessage) {
+    xubinh_server::LogCollector::set_base_name("hello_world");
+
     LOG_INFO << "hello, world!";
 }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+
     return RUN_ALL_TESTS();
 }
