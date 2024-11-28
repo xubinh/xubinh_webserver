461fb56b5d9869e2c9929793b28550d73814946f
xubinh <xubin.huang@outlook.com>
2024-11-28 13:11:22 +0800

    🔧 Refine basic test for logging

--- test/test_logging.cc
+++ test/test_logging.cc
@@ -16,17 +16,25 @@ TEST(LogBuilderTest, BasicLogMessage2) {
 
     double d = 3.14;
 
-    char *a = new char[32];
+    char *a = new char[32]{"123456"};
+
+    int *p = new int;
 
     LOG_INFO << i;
     LOG_INFO << d;
     LOG_INFO << a;
+    LOG_INFO << p;
+
+    delete p;
 
     delete a;
 }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+
+    // Disable output capturing
+    testing::FLAGS_gtest_catch_exceptions = false;
 
     return RUN_ALL_TESTS();
 }
