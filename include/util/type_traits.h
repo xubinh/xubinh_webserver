5a2dc9647801392a372c7558ab8aa1ee24be4e93
xubinh <xubin.huang@outlook.com>
2024-11-28 11:03:19 +0800

    🔧 Add helper for checking type equality

--- include/util/type_traits.h
+++ include/util/type_traits.h
@@ -13,6 +13,16 @@ namespace type_traits {
 template <bool C, typename T = void>
 using enable_if_t = typename std::enable_if<C, T>::type;
 
+template <typename T1, typename T2>
+struct is_same_type {
+    static constexpr bool value = false;
+};
+
+template <typename T>
+struct is_same_type<T, T> {
+    static constexpr bool value = true;
+};
+
 } // namespace type_traits
 
 } // namespace util
