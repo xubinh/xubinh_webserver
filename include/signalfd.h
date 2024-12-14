ca61504a415c0edec9123948d0a7418f60bd45fa
xubinh <xubin.huang@outlook.com>
2024-12-14 19:30:54 +0800

    ✨ Implement `Signalfd`

---
+++ include/signalfd.h
@@ -0,0 +1,79 @@
+#ifndef XUBINH_SERVER_SIGNALFD
+#define XUBINH_SERVER_SIGNALFD
+
+#include <csignal>
+#include <functional>
+#include <sys/signalfd.h>
+
+#include "event_loop.h"
+#include "pollable_file_descriptor.h"
+
+namespace xubinh_server {
+
+class SignalSet {
+public:
+    SignalSet() {
+        ::sigemptyset(&_signal_set);
+    }
+
+    void add_signal(int signal);
+
+    void remove_signal(int signal);
+
+private:
+    friend class Signalfd;
+
+    sigset_t _signal_set{};
+};
+
+class Signalfd {
+public:
+    using SignalDispatcherType = std::function<void(int signal)>;
+
+    // only blocks calling thread's signals
+    static void block_signals(SignalSet signals_to_block);
+
+    // only unblocks calling thread's signals
+    static void unblock_signals(SignalSet signals_to_unblock);
+
+    static int create_signalfd(SignalSet initial_listening_set, int flags);
+
+    Signalfd(int fd, EventLoop *loop) : _pollable_file_descriptor(fd, loop) {
+        if (fd < 0) {
+            LOG_SYS_FATAL << "invalid file descriptor (must be non-negative)";
+        }
+
+        _pollable_file_descriptor.register_read_event_callback(
+            std::bind(_read_event_callback, this)
+        );
+    }
+
+    ~Signalfd() {
+        _pollable_file_descriptor.disable_read_event();
+    }
+
+    void register_signal_dispatcher(SignalDispatcherType signal_dispatcher) {
+        _signal_dispatcher = std::move(signal_dispatcher);
+    }
+
+    void start() {
+        if (!_signal_dispatcher) {
+            LOG_FATAL << "missing signal dispatcher";
+        }
+
+        _pollable_file_descriptor.enable_read_event();
+    }
+
+private:
+    static constexpr int _DEFAULT_SIGNALFD_FLAGS = SFD_NONBLOCK | SFD_CLOEXEC;
+
+    void _read_event_callback();
+
+    SignalDispatcherType _signal_dispatcher;
+
+    PollableFileDescriptor _pollable_file_descriptor;
+};
+
+} // namespace xubinh_server
+
+#endif
---
+++ src/signalfd.cc
@@ -0,0 +1,80 @@
+#include "signalfd.h"
+#include "log_builder.h"
+
+namespace xubinh_server {
+
+void SignalSet::add_signal(int signal) {
+    if (signal == SIGKILL || signal == SIGSTOP) {
+        LOG_FATAL << "tried to add SIGKILL and SIGSTOP to a signal set";
+    }
+
+    ::sigaddset(&_signal_set, signal);
+}
+
+void SignalSet::remove_signal(int signal) {
+    if (signal == SIGKILL || signal == SIGSTOP) {
+        LOG_WARN << "tried to remove SIGKILL and SIGSTOP from a signal set";
+    }
+
+    ::sigdelset(&_signal_set, signal);
+}
+
+void Signalfd::block_signals(SignalSet signals_to_block) {
+    if (::sigprocmask(SIG_BLOCK, &signals_to_block._signal_set, nullptr)) {
+        LOG_SYS_FATAL << "unknown error when blocking signals";
+    }
+}
+
+void Signalfd::unblock_signals(SignalSet signals_to_unblock) {
+    if (::sigprocmask(SIG_UNBLOCK, &signals_to_unblock._signal_set, nullptr)) {
+        LOG_SYS_FATAL << "unknown error when unblocking signals";
+    }
+}
+
+int Signalfd::create_signalfd(SignalSet initial_listening_set, int flags) {
+    if (!flags) {
+        flags = _DEFAULT_SIGNALFD_FLAGS;
+    }
+
+    auto fd = ::signalfd(-1, &initial_listening_set._signal_set, flags);
+
+    if (fd == -1) {
+        if (errno == EINVAL) {
+            LOG_FATAL << "invalid flags are given to create signalfd";
+        }
+        else {
+            LOG_SYS_FATAL << "failed when trying to create signalfd";
+        }
+    }
+
+    return fd;
+}
+
+void Signalfd::_read_event_callback() {
+    struct signalfd_siginfo signal_info;
+
+    // one signal at a time
+    while (true) {
+        auto bytes_read = ::read(
+            _pollable_file_descriptor.get_fd(),
+            &signal_info,
+            sizeof(signal_info)
+        );
+
+        if (bytes_read != sizeof(signal_info)) {
+            // all signals in the pending queue are read
+            if (errno == EAGAIN) {
+                break;
+            }
+
+            else {
+                LOG_SYS_FATAL << "unknown error occured when trying to read "
+                                 "from signalfd";
+            }
+        }
+
+        _signal_dispatcher(static_cast<int>(signal_info.ssi_signo));
+    }
+}
+
+} // namespace xubinh_server
