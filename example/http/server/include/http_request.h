8ef484718a6fa1f5d55a96563065e5cc76cf5a1e
xubinh <xubin.huang@outlook.com>
2024-12-26 16:58:34 +0800

    🔧 Implement move constructors for `HttpRequest` just for sense of security

--- example/http/server/include/http_request.h
+++ example/http/server/include/http_request.h
@@ -25,8 +25,27 @@ public:
     // dummy; abort if get called
     HttpRequest &operator=(const HttpRequest &);
 
-    HttpRequest(HttpRequest &&) = default;
-    HttpRequest &operator=(HttpRequest &&) = default;
+    // implement this anyway just for sense of security
+    HttpRequest(HttpRequest &&other)
+        : _method(other._method), _path(std::move(other._path)),
+          _version(other._version),
+          _receive_time_point(other._receive_time_point),
+          _headers(std::move(other._headers)), _body(std::move(other._body)) {
+    }
+
+    // implement this anyway just for sense of security
+    HttpRequest &operator=(HttpRequest &&other) {
+        if (this != &other) {
+            _method = other._method;
+            _path = std::move(other._path);
+            _version = other._version;
+            _receive_time_point = other._receive_time_point;
+            _headers = std::move(other._headers);
+            _body = std::move(other._body);
+        }
+
+        return *this;
+    }
 
     bool parse_request_line(const char *start, const char *end);
 
